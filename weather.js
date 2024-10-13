6bfb98c040cd66657591339f278fd5fa01d1c70b
adil-shafi <adil.shafi0504@gmail.com>
2024-10-13 23:11:33 +0100

    Create weather.js

---
+++ weather.js
@@ -0,0 +1,3 @@
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button"); 
+const weatherIcon = document.querySelector(".weather-icon");
